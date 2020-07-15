f393247bf91c9259959d8ae63a60ad330489cd13
xthecoolboy <67747753+xthecoolboy@users.noreply.github.com>
2020-07-15 12:21:28 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-token = '' #Put Your bots token here
+token = 'NzIwOTY3NjkxMzcyMDY4ODc0.Xw7VAg.wSHXcyUtrha9nknSB6v9keaVu4I' #Put Your bots token here
 
-prefix = '' #put prefix here
+prefix = '+' #put prefix here
 
-link = '' #put bot invite link here
+link = 'https://discord.gg/atcRGK7' #put bot invite link here
 
-ownerid = '' #put your id here
+ownerid = '376384142599782410' #put your id here
 
